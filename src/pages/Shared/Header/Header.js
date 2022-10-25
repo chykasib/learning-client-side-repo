842a88fedff9b5d67249a340343c09f9552e407c
Kasib Mohammad Chowdhury <kasib.md.chy@gmail.com>
2022-10-25 20:16:58 +0600

    add tooltip

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -4,6 +4,8 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Image from 'react-bootstrap/Image'
 import { Link } from 'react-router-dom';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 import { Button } from 'react-bootstrap';
 import { useContext } from 'react';
 import { AuthContext } from '../../../Context/AuthProvider';
@@ -14,9 +16,11 @@ const Header = () => {
         signUp()
             .then(() => { })
             .catch(error => console.error())
+
+
     }
     return (
-        <Navbar className='shadow p-3 mb-5' collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar className='shadow-lg p-3 mb-5' collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
                 <Navbar>
                     <Image
@@ -24,7 +28,6 @@ const Header = () => {
                         style={{ height: '50px' }}
                         roundedCircle
                     >
-
                     </Image>
                 </Navbar>
                 <Navbar className='text-info fs-3 ms-4 pe-3'>Keystone Cse Courses</Navbar>
@@ -42,11 +45,28 @@ const Header = () => {
                                     <Link className='text-decoration-none text-light px-3 fs-4' to={'/login'} eventKey={2}>
                                         <Button onClick={signUpHandler} variant="warning">Sign Up </Button>
                                     </Link>
-                                    <Image
-                                        src={user?.photoURL}
-                                        style={{ height: '50px' }}
-                                        roundedCircle
-                                    ></Image>
+                                    <OverlayTrigger
+                                        placement="bottom"
+                                        overlay={<Tooltip id="button-tooltip-2">{user?.displayName}</Tooltip>}
+                                    >
+                                        {({ ref, ...triggerHandler }) => (
+
+                                            <Image
+                                                variant="light"
+                                                {...triggerHandler}
+                                                className="d-inline-flex align-items-center"
+                                                ref={ref}
+
+                                                src={user?.photoURL}
+                                                style={{ height: '50px' }}
+                                                roundedCircle
+                                            ></Image>
+
+
+
+                                        )}
+                                    </OverlayTrigger>
+
                                 </>
                                 : <Link className='text-decoration-none text-light px-3 fs-4' to={'/login'} eventKey={2}>
                                     <Button variant="warning">Login </Button>
